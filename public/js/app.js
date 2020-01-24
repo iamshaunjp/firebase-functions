5c69c3a9ed8f1c8aab1915cb34a26b02c3d3066f
Shaun <shaun@thenetninja.co.uk>
2020-01-24 14:05:19 +0000

    lesson-11

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const requestModal = document.querySelector('.new-request');
 const requestLink = document.querySelector('.add-request');
+const requestForm = document.querySelector('.new-request form');
 
 // open request modal
 requestLink.addEventListener('click', () => {
@@ -11,4 +12,22 @@ requestModal.addEventListener('click', (e) => {
   if (e.target.classList.contains('new-request')) {
     requestModal.classList.remove('open');
   }
+});
+
+// add a new request
+requestForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const addRequest = firebase.functions().httpsCallable('addRequest');
+  addRequest({ 
+    text: requestForm.request.value 
+  })
+  .then(() => {
+    requestForm.reset();
+    requestForm.querySelector('.error').textContent = '';
+    requestModal.classList.remove('open');
+  })
+  .catch(error => {
+    requestForm.querySelector('.error').textContent = error.message;
+  });
 });
