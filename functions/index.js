f4b5c61bffdddaffdd18220f8620a7d20b975e74
Shaun <shaun@thenetninja.co.uk>
2020-01-24 13:45:41 +0000

    lesson-8

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,11 @@
 const functions = require('firebase-functions');
 
-// http request 1
-exports.randomNumber = functions.https.onRequest((request, response) => {
-  const number = Math.round(Math.random() * 100);
-  console.log(number);
-  response.send(number.toString());
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate(user => {
+  console.log('user created: ', user.email, user.uid);
 });
 
-// http request 2
-exports.toTheDojo = functions.https.onRequest((request, response) => {
-  response.redirect('https://www.thenetninja.co.uk');
-});
-
-// http callable function
-exports.sayHello = functions.https.onCall((data, context) => {
-  const name = data.name;
-  return `hello ${name} :)`;
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete(user => {
+  console.log('user deleted: ', user.email, user.uid);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -12,14 +12,3 @@ requestModal.addEventListener('click', (e) => {
     requestModal.classList.remove('open');
   }
 });
-
-// say hello function call
-const button = document.querySelector('.call');
-button.addEventListener('click', () => {
-// get function reference
-  const sayHello = firebase.functions().httpsCallable('sayHello');
-  // call the function and pass data
-  sayHello({ name: 'Shaun' }).then(result => {
-    console.log(result.data);
-  });
-});
