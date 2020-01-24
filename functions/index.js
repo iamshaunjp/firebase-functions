41bc74e18f1ff18f2b609dac88a8be21a15cb0da
Shaun <shaun@thenetninja.co.uk>
2020-01-24 13:56:36 +0000

    lesson-9

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // auth trigger (new user signup)
 exports.newUserSignUp = functions.auth.user().onCreate(user => {
-  console.log('user created: ', user.email, user.uid);
+  // for background triggers you must return a value/promise
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  });
 });
 
 // auth trigger (user deleted)
 exports.userDeleted = functions.auth.user().onDelete(user => {
-  console.log('user deleted: ', user.email, user.uid);
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
 });
