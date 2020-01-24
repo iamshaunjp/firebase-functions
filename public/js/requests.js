91fa03cfd3ddd61539e1ac8d365bbba85e3f1ca6
Shaun <shaun@thenetninja.co.uk>
2020-01-24 16:27:40 +0000

    lesson-13

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,17 +1,16 @@
-const ref = firebase.firestore().collection('requests');
-
-ref.onSnapshot(snapshot => {
-  // console.log(snapshot);
-
-  let requests = [];
-  snapshot.forEach(doc => {
-    requests.push({...doc.data(), id: doc.id});
-  });
-  // console.log(requests);
-
-  let html = '';
-  requests.forEach(request => {
-    html += `<li>${request.text}</li>`
-  });
-  document.querySelector('ul').innerHTML = html;
+var app = new Vue({
+  el: '#app',
+  data: {
+    requests: [],
+  },
+  mounted() {
+    const ref = firebase.firestore().collection('requests');
+    ref.onSnapshot(snapshot => {
+      let requests = [];
+      snapshot.forEach(doc => {
+        requests.push({...doc.data(), id: doc.id});
+      });
+      this.requests = requests;
+    });
+  }
 });
