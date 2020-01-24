d1510d41b047793ae6632eeaf894fa8937cec4a9
Shaun <shaun@thenetninja.co.uk>
2020-01-24 13:59:36 +0000

    lesson-10

--- functions/index.js
+++ functions/index.js
@@ -16,3 +16,23 @@ exports.userDeleted = functions.auth.user().onDelete(user => {
   const doc = admin.firestore().collection('users').doc(user.uid);
   return doc.delete();
 });
+
+// http callable function (adding a request)
+exports.addRequest = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated', 
+      'only authenticated users can add requests'
+    );
+  }
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+      'invalid-argument', 
+      'request must be no more than 30 characters long'
+    );
+  }
+  return admin.firestore().collection('requests').add({
+    text: data.text,
+    upvotes: 0
+  });
+});
