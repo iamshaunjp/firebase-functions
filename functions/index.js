e2bbefb9e18c053fca4e3ef3b8cae420d9b17386
Shaun <shaun@thenetninja.co.uk>
2020-01-24 16:59:26 +0000

    lesson-18

--- functions/index.js
+++ functions/index.js
@@ -63,10 +63,28 @@ exports.upvote = functions.https.onCall(async (data, context) => {
   await user.update({
     upvotedOn: [...doc.data().upvotedOn, data.id]
   });
-  
+
   // update the votes on the request
   return request.update({
     upvotes: admin.firestore.FieldValue.increment(1)
   });
 
+});
+
+// firestore trigger for tracking activity
+exports.logActivities = functions.firestore.document('/{collection}/{id}')
+  .onCreate((snap, context) => {
+    console.log(snap.data());
+
+    const activities = admin.firestore().collection('activities');
+    const collection = context.params.collection;
+
+    if (collection === 'requests') {
+      return activities.add({ text: 'a new tutorial request was added' });
+    }
+    if (collection === 'users') {
+      return activities.add({ text: 'a new user signed up'});
+    }
+
+    return null;
 });
