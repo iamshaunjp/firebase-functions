afa7bfbfbd2fc6ff09b79b956556531fd63894d1
Shaun <shaun@thenetninja.co.uk>
2020-01-24 11:40:40 +0000

    lesson-4

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// http request 1
+exports.randomNumber = functions.https.onRequest((request, response) => {
+  const number = Math.round(Math.random() * 100);
+  console.log(number);
+  response.send(number.toString());
+});
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((request, response) => {
+  response.redirect('https://www.thenetninja.co.uk');
+});
